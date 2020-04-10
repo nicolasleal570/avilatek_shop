62d500ab62468f757b75693f8a8335726dc3255e
nicolasleal570 <lealnicolas96@gmail.com>
2020-04-10 04:10:03 -0400

    Adding TailwindCSS

--- src/App.js
+++ src/App.js
@@ -3,38 +3,39 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { connect } from "react-redux";
 import BaseRouter from "./routes";
 import * as actions from "./store/actions/auth";
-import "semantic-ui-css/semantic.min.css";
 import CustomLayout from "./containers/Layout";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
+import './assets/main.css';
 
-  render() {
-    return (
-      <Router>
-        <CustomLayout {...this.props}>
-          <BaseRouter />
-        </CustomLayout>
-      </Router>
-    );
-  }
+class App extends Component {
+	componentDidMount() {
+		this.props.onTryAutoSignup();
+	}
+
+	render() {
+		return (
+			<Router>
+				<CustomLayout {...this.props}>
+					<BaseRouter />
+				</CustomLayout>
+			</Router>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.auth.token !== null
-  };
+	return {
+		isAuthenticated: state.auth.token !== null
+	};
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSignup: () => dispatch(actions.authCheckState())
-  };
+	return {
+		onTryAutoSignup: () => dispatch(actions.authCheckState())
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(App);
